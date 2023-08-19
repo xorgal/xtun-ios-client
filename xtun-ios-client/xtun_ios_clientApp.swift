b1459d0f5882a0901090b427bb4b75feccd30ef8
xorgal <93151521+xorgal@users.noreply.github.com>
2023-08-19 17:51:12 +0300

    Initial Commit

---
+++ xtun-ios-client/xtun_ios_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  xtun_ios_clientApp.swift
+//  xtun-ios-client
+//
+//  Created by xorgal on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct xtun_ios_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
